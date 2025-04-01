9d7d761ca860c4e4c3dafe483c70f91c67b4fd0e
Vinicius Bisol <vinybisol@gmail.com>
2025-03-31 22:51:42 -0300

    several changes

--- src/app/pages/register/general-form/general-form.component.ts
+++ src/app/pages/register/general-form/general-form.component.ts
@@ -1,15 +1,17 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-general-form',
-  imports: [MatInputModule, MatFormFieldModule, ReactiveFormsModule, MatDatepickerModule, MatCardModule, MatSelectModule],
+  imports: [CommonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule, MatDatepickerModule, MatCardModule, MatSelectModule],
   templateUrl: './general-form.component.html',
   styleUrl: './general-form.component.scss',
   providers: [provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }],
